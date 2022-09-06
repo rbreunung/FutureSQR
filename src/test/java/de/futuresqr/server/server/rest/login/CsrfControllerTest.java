c9a1344d3cff9afbcec66be0554e3686fb688be6
Robert Breunung <rbreunung@gmail.com>
2022-09-06 23:47:23 +0200

    First test for CSRF endpoint.

--- src/test/java/de/futuresqr/server/server/rest/login/CsrfControllerTest.java
+++ src/test/java/de/futuresqr/server/server/rest/login/CsrfControllerTest.java
@@ -1,5 +1,10 @@
 package de.futuresqr.server.server.rest.login;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,12 +24,56 @@ public class CsrfControllerTest {
 
 	@LocalServerPort
 	int serverPort;
-	
+
 	@Test
-	public void test() {
+	public void getCsrf_validUrl_tokenPresent() throws Exception {
+
+		ResponseEntity<CsrfDto> entity = webclient.getForEntity("http://localhost:" + serverPort + "/rest/login/csrf",
+				CsrfDto.class);
+
+		CsrfDto json = entity.getBody();
+		assertNotNull(json.getToken(), "CSRF token expected");
+	}
+
+	@Test
+	public void getCsrf_validUrl_headerNamePresent() throws Exception {
+
+		ResponseEntity<CsrfDto> entity = webclient.getForEntity("http://localhost:" + serverPort + "/rest/login/csrf",
+				CsrfDto.class);
+
+		CsrfDto json = entity.getBody();
+		assertNotNull(json.getHeaderName(), "CSRF header name expected");
+	}
+
+	@Test
+	public void getCsrf_validUrl_paramNamePresent() throws Exception {
+
+		ResponseEntity<CsrfDto> entity = webclient.getForEntity("http://localhost:" + serverPort + "/rest/login/csrf",
+				CsrfDto.class);
+
+		CsrfDto json = entity.getBody();
+		assertNotNull(json.getParameterName(), "CSRF param name expected");
+	}
+
+	@Test
+	public void getCsrf_validRequest_cookiePresent() {
 		ResponseEntity<String> entity = webclient.getForEntity("http://localhost:" + serverPort + "/rest/login/csrf",
 				String.class);
-		
+
+		List<String> list = entity.getHeaders().get("Set-Cookie");
+
+		assertEquals(1, list.size(), "Expect a cookie set request.");
+
+		//TODO next test shall provide this for Login
+		String result = list.get(0).split(";")[0];
+		log.info(result);
+	}
+
+	@Test
+	public void getCsrf() {
+		ResponseEntity<String> entity = webclient.getForEntity("http://localhost:" + serverPort + "/rest/login/csrf",
+				String.class);
+		log.info(entity.getHeaders().toString());
 		log.info(entity.getBody());
 	}
 }
